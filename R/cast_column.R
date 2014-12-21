dc9739407145dbb8ccb7504c48fadd9960393767
djvanderlaan <djvanderlaan@unrealizedtime.nl>
2014-12-21 13:29:40 +0100

    Fixed bug in cast_column

--- R/cast_column.R
+++ R/cast_column.R
@@ -1,7 +1,6 @@
 
 cast_column <- function(x, meta) {
   type <- meta$type
-  format <- meta$format
   if (!is.character(type)) stop("type should be a character vector.")
   type <- type[1]
   fun <- paste0("cast_column_", type)
@@ -9,7 +8,7 @@ cast_column <- function(x, meta) {
     warning("Unsupported field type: '", type, "'; reading as character field")
     res <- x
   } else {
-    res <- do.call(fun, list(x, format))
+    res <- do.call(fun, list(x, meta))
   }
   if (!is.null(meta$categories)) {
     levels <- sapply(meta$categories, function(d) d$name)
