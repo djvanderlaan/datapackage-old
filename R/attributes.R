06dfe5baad7e98d3f0dc5bd322f611c669a0ba93
djvanderlaan <djvanderlaan@unrealizedtime.nl>
2014-12-23 12:16:47 +0100

    Added dpattr<- methods and tests for attributes

--- R/attributes.R
+++ R/attributes.R
@@ -5,6 +5,7 @@
 #'   This can be an object of type \code{\link{datapackage}},
 #'   \code{\link{dpresource}} or \code{\link{dpfield}}. 
 #' @param attribute the name of the attribute that needs to be set. 
+#' @param value the value to which the attribute should be set.
 #' @param ... currently unused. 
 #'
 #' @details
@@ -48,6 +49,37 @@ dpattr.dpfield <- function(meta, attribute, ...) {
   meta[[attribute]]
 }
 
+#' @rdname attributes
+#' @export
+`dpattr<-` <- function(meta, attribute, value) {
+  if (!is.character(attribute) || length(attribute) != 1) 
+    stop("attribute needs to be a character vector of length 1.")
+  UseMethod("dpattr<-")
+}
+
+#' @rdname attributes
+#' @export dpattr<-.datapackage
+`dpattr<-.datapackage` <- function(meta, attribute, value) {
+  meta[[attribute]] <- value
+  meta
+}
+
+#' @rdname attributes
+#' @export dpattr<-.dpresource
+`dpattr<-.dpresource` <- function(meta, attribute, value) {
+  meta[[attribute]] <- value
+  meta
+}
+
+#' @rdname attributes
+#' @export dpattr<-.dpfield
+`dpattr<-.dpfield` <- function(meta, attribute, value) {
+  meta[[attribute]] <- value
+  meta
+}
+
+
+
 
 
 # ======= Some shortcuts =======
@@ -75,7 +107,7 @@ dpdescription <- function(meta, ...) {
 dptype <- function(meta, ...) {
   if (!is(meta, "dpfield")) 
     stop("Only fields have a type. Meta should be of type 'dpfield'.")
-  dpattr(meta, "type")
+  dpattr(meta, attribute="type", ...)
 }
 
 #' @rdname attributes
@@ -83,6 +115,45 @@ dptype <- function(meta, ...) {
 dpformat <- function(meta, ...) {
   if (!is(meta, "dpfield")) 
     stop("Only fields have a format. Meta should be of type 'dpfield'.")
-  dpattr(meta, "format")
+  dpattr(meta, attribute="format", ...)
+}
+
+#' @rdname attributes
+#' @export
+`dptitle<-` <- function(meta, value) {
+  dpattr(meta, attribute="title") <- value
+  meta
+}
+
+#' @rdname attributes
+#' @export
+`dpname<-` <- function(meta, value) {
+  dpattr(meta, attribute="name") <- value
+  meta
+}
+
+#' @rdname attributes
+#' @export
+`dpdescription<-` <- function(meta, value) {
+  dpattr(meta, attribute="description") <- value
+  meta
+}
+
+#' @rdname attributes
+#' @export
+`dptype<-` <- function(meta, value) {
+  if (!is(meta, "dpfield")) 
+    stop("Only fields have a type. Meta should be of type 'dpfield'.")
+  dpattr(meta, "type") <- value
+  meta
+}
+
+#' @rdname attributes
+#' @export
+`dpformat<-` <- function(meta, value) {
+  if (!is(meta, "dpfield")) 
+    stop("Only fields have a format. Meta should be of type 'dpfield'.")
+  dpattr(meta, "format") <- value
+  meta
 }
 
---
+++ tests/test_attributes.R
@@ -0,0 +1,30 @@
+library(testthat)
+library(datapackage)
+
+pkg <- datapackage(data=iris)
+
+expect_that(dpname(pkg), equals("iris"))
+expect_that(dptitle(pkg), equals("iris"))
+expect_that(dpdescription(pkg), equals(""))
+expect_that(dpattr(pkg, "name"), equals("iris"))
+
+expect_error(dptype(pkg))
+expect_error(dpformat(pkg))
+expect_error(dpattr(pkg, 1))
+expect_error(dpattr(pkg, c("name", "title")))
+
+dptitle(pkg) <- "Iris"
+expect_that(pkg[["title"]], equals("Iris"))
+
+dpattr(pkg, "foobar") <- "Foo Bar"
+expect_that(pkg[["foobar"]], equals("Foo Bar"))
+expect_that(dpattr(pkg, "foobar"), equals("Foo Bar"))
+
+
+fld <- dpfield(pkg, "Sepal.Length")
+expect_that(dptype(fld), equals("number"))
+expect_that(dpformat(fld), equals(NULL))
+
+dpformat(fld) <- "foo bar"
+expect_that(fld[["format"]], equals("foo bar"))
+
